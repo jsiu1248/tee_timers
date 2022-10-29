16d30d819c0366e724a3d36ccbbedbbf18a540d2
Jonathan Siu <jsiu1248@gmail.com>
2022-10-29 11:33:17 -0700

    added configs and initializated classes

---
+++ app/__init__.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+import os
+from config import configs
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_wtf.csrf import CSRFProtect
+
+# absolute path
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# instances of classes
+db = SQLAlchemy()
+bootstrap = Bootstrap()
+migrate = Migrate()
+mail = Mail()
+moment = Moment()
+# protects against cross-site request forgery outside of forms
+csrf = CSRFProtect()
+login_manager = LoginManager()
+
+def create_app(config_name='default'):
+    """ Creating Flask instance and initializing 
+    
+    """
+    # __name__ shortcuts to using the name of the package
+    app = Flask(__name__)
+
+    # loading configs before loading extensions
+    config_class = configs[config_name]
+    app.config.from_object(config_class)
+
+    # initializing app with extensions
+    config_class.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bootstrap.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    csrf.init_app(app)
---
+++ config.py
@@ -0,0 +1,60 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SQLALCHEMY_DATABASE_URI =\
+         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'anyRandomLongStringUseNumbersYo#123'
+        # Flask-Mail config
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    COMPS_PER_PAGE = 25
+    FOLLOWERS_PER_PAGE = 25
+    # Other email settings
+    # email for the administrator of the flask app
+    APP_ADMIN = os.environ.get('APP_ADMIN')
+
+    # this will be the prefix everytime an emails is sent
+    MAIL_SUBJECT_PREFIX = 'App — '
+
+    # when an email is sent to a user, it is set to this value
+    APP_MAIL_SENDER = f'App Admin <{APP_ADMIN}>'
+    HTTPS_REDIRECT = False
+
+    # export MAIL_USERNAME=<your Gmail username>
+    # remember to change it to your app password
+    # export MAIL_PASSWORD=<your Gmail app password>
+    # don't include spaces. It didn't like it
+
+    @staticmethod
+    def init_app(app):
+        pass
+configs = {'default': Config}
+
+# getting breakpoints, traces, and reloaders
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_DEV_URL') or \
+         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+# for unit testing
+class TestingConfig(Config):
+    # making sure that these have names related to testing
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL') or \
+'sqlite:///{os.path.join(basedir, "data-test.sqlite")}'
+
+
+
+# giving them all names
+configs = {
+     'development': DevelopmentConfig,
+     'testing': TestingConfig,
+     'default': DevelopmentConfig 
+}
