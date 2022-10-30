51373f8a941760156885653ff75b1c6bdb3b7f81
Jonathan Siu <jsiu1248@gmail.com>
2022-10-29 22:31:06 -0700

    added create app instance and shell_context_prossor

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ from app import create_app, db
 import os
 
 from flask_migrate import upgrade
+
+# creating create_app instance
+app = create_app(os.environ.get("FLASK_CONFIG") or "default")
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db)
