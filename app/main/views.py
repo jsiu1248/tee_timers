65a29308e14b55082cc1544d6ee9490c7a9a5e13
Jonathan Siu <jsiu1248@gmail.com>
2022-10-30 15:30:42 -0700

    added index

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,10 @@
 from . import main
 from flask import render_template, session, redirect, url_for, flash, current_app, request, abort, make_response
-from .forms import NameForm, EditProfileForm, AdminLevelEditProfileForm, CompositionForm # need a period because trying to import within package
 from .. import db
 from flask_login import login_required, current_user
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    return "hello"
