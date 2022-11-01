1667fed03a38295173f4978f3f8f7924a1399d7d
Jonathan Siu <jsiu1248@gmail.com>
2022-11-01 10:51:03 -0700

    added auth __init__

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,3 +1,5 @@
 from flask import render_template,  Blueprint
 
 api = Blueprint('api' ,__name__ , url_prefix="/api/v1") 
+
+# from . import authentication, comments, compositions, errors, users
---
+++ app/auth/__init__.py
@@ -0,0 +1,5 @@
+from flask import render_template,  Blueprint
+
+auth = Blueprint('auth' ,__name__ , url_prefix="/auth") 
+
+# from . import views
