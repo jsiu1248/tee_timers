f730aa9d678172e685a19ae430bb6037dd54a0a3
Jonathan Siu <jsiu1248@gmail.com>
2022-11-03 11:32:09 -0400

    adding reoute logic and page_content to base because it wasn't showing in login.html

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,3 +1,4 @@
+import email
 from flask import render_template, session, g, redirect, url_for, flash, current_app, request
 
 # from app.email import send_email
@@ -14,13 +15,54 @@ from . import auth
 def login():
     # form is created
     form = LoginForm()
+
+    """email_entered = form.email.data
+    password_entered = form.password.data
+    # query checking if the name is in the database
+    user = User.query.filter_by(email = email_entered).first()
+    # if user exists and the password is correct
+    if user and user.verify_password(password_entered):
+        login_user(user, form.remember_me.data)
+        next = request.args.get('next')
+
+        if next is None or not next.startswith('/'):
+            next = url_for('main.index')
+        return redirect(next)
+        # flash a message that username/password is invalid
+    flash("The username/password is invalid")"""
+
+
     return render_template("auth/login.html", form = form)
 
 @auth.route('/register', methods=["GET","POST"])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        email_entered = form.email.data
+    password_entered = form.password.data
+    username_entered = form.username.data
+    u = User(email = email_entered, username = username_entered, 
+        password = password_entered)
 
+    db.session.add(u)
+    db.session.commit()
+    flash("You can now login.")
+    
+    # generating token for user
+    # token = u.generate_confirmation_token()
+
+    #     # _external = True in Flask Mail tells it to generate an absolute link
+    # confirmation_link = url_for('auth.confirm', token = token, _external = True)
+    # # send_email(u.email, 'Welcome to Ragtime!', 'mail/welcome', user = u)
+    # send_email(u.email, "Confirmation email!", 'auth/confirm', user = u, confirmation_link = confirmation_link)
+    # flash("A confirmation email was send to you.")
+
+        # return redirect(url_for('main.index'))
     return render_template('auth/register.html', form = form)
+
+
+
+
 
 @auth.route('/logout')
 def logout():
@@ -34,4 +76,19 @@ def change_password():
     Returns: renders change password form and change-password.
     """
     form = ChangePasswordForm()
+    if form.validate_on_submit():
+        password = form.password.data
+    new_password = form.new_password.data
+    # Checks if old password matches then we can use the new password
+    if current_user.verify_password(password) == True:
+        current_user.password = new_password
+        # add it to the current user and commit
+        db.session.add(current_user)
+        db.session.commit()
+        flash('Password has been changed.')
+        # the password change was a success, so user is directed to login
+        return redirect(url_for('auth.login'))
+    else:
+        flash('Old password does not match. Try again.')
+
     return render_template('auth/change_password.html', form = form)
--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from .. import db
 from flask_login import login_required, current_user
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from ..models import User, Role
+from ..models import User, Role, Permission
 from ..decorators import permission_required, admin_required
 
 @main.route('/', methods=['GET', 'POST'])
@@ -15,4 +15,15 @@ def index():
 @login_required
 @admin_required
 def for_admins_only():
+    """route will require login and user to have admin permission"""
     return f"Welcome, Administrator! {Permission.ADMIN}"
+
+
+@main.route('/moderate')
+@login_required
+@permission_required(Permission.MODERATE)
+def for_moderators_only():
+    """
+    route will require login and the user to have moderate permissions
+    """
+    return "Greetings, moderator!"
