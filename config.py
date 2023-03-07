46ed98b9abb4380a569c22beed164c0bfcdd9f21
Jonathan Siu <jsiu1248@gmail.com>
2023-03-06 23:13:24 -0800

    editting url to database

--- config.py
+++ config.py
@@ -54,8 +54,13 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #     f'sqlite:///{os.path.join(basedir, "data.sqlite")}'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         f'sqlite:///{os.path.join(basedir, "data.sqlite")}'
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
 
     @classmethod
     def init_app(cls, app):
