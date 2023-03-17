7f59278bf21ef2cce2efccd2008ad3d7a40d409e
Jonathan Siu <jsiu1248@gmail.com>
2023-03-16 22:53:53 -0700

    editting comment for make_shell_context

--- golf_app.py
+++ golf_app.py
@@ -6,10 +6,12 @@ from flask_migrate import upgrade
 
 # creating create_app instance
 app = create_app(os.environ.get("FLASK_CONFIG") or "default")
-
-# adding variables to the shell context so that it doesn't need to be imported again
+ 
 @app.shell_context_processor
 def make_shell_context():
+    """
+    adding variables to the shell context so that it doesn't need to be imported again
+    """
     return dict(db = db, Role = Role, User = User, Comment = Comment, 
     Follow = Follow, Post = Post, City = City, State = State
     )
