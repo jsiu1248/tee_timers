e549e35f9a8de6d848ba9a2e20e0ba7a66e8a6b8
Jonathan Siu <jsiu1248@gmail.com>
2023-03-21 23:15:37 -0700

    editting comment in init

--- app/__init__.py
+++ app/__init__.py
@@ -64,7 +64,6 @@ def create_app(config_name='default'):
                     'cdnjs.cloudflare.com', 'cdn.jsdelivr.net' ,'127.0.0.1:5000' ,'teetimers.herokuapp.com',
                 ],
                 # allow images from anywhere, 
-                #   including unicornify.pictures
                 'img-src': '*'
             }
         )
