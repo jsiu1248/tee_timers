93b5e3e8ac47da27c39eb5aadc271b6e32e36ce6
Jonathan Siu <jsiu1248@gmail.com>
2023-04-19 11:09:54 -0700

    I had two flash-messages function. So, I commented out one of them. Also, fixed time out by adding it to the correct function

--- app/static/js/timeOut.js
+++ app/static/js/timeOut.js
@@ -1,5 +1,12 @@
 setTimeout(function() {
     // console.log('are you working');
     $('.flash-message').fadeOut();
+    document.querySelector('.alert warning').remove();
+    
 }, 5000); // 5000 milliseconds = 5 seconds
 // console.log('yes')
+
+
+// setTimeout(function() {
+//     document.querySelector('.alert-info').remove();
+//   }, 5000);
