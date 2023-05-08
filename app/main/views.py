ddc6e31f7bb9f2791fd8e5d72380bd2d884b49d9
Jonathan Siu <jsiu1248@gmail.com>
2023-05-07 23:18:16 -0700

    so, golf_log.action in models is None right now. Trying to figure out why it isn't seeing the relationship between Action and action_id

--- app/main/views.py
+++ app/main/views.py
@@ -850,6 +850,7 @@ def golf_log_form():
     if golf_log_form.validate_on_submit():
         # save post to database
         golf_log = GolfLog(action_id = golf_log_form.action.data, satisfaction_level_id = golf_log_form.satisfaction.data, user_id=current_user.id)
+        print(golf_log_form.action.data)
         points = calculate_points(golf_log)
         db.session.add(golf_log)
         db.session.commit()
