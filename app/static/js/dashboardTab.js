ea1d7ac3e127bf6a3ad27e0216a604ec596328d8
Jonathan Siu <jsiu1248@gmail.com>
2023-05-12 18:02:08 -0700

    removed prevent default because it stays on the same page. Added conditional logic so that it changes which tab is active.

--- app/static/js/dashboardTab.js
+++ app/static/js/dashboardTab.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
   
     // Switch to the tab that was clicked
     $('.nav-tabs a').on('click', function(e) {
-      e.preventDefault();
       $(this).tab('show');
     });
   });
