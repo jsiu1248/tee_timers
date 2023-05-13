48ff89f704d4031f508c7a8e63a06eea989732a5
Jonathan Siu <jsiu1248@gmail.com>
2023-05-13 16:24:48 -0700

    changed logic of dashboardTab. It was all referring to the same title. So, the name needed to be changed in order to point it to the correct active tab.

--- app/static/js/dashboardTab.js
+++ app/static/js/dashboardTab.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
-    // Activate the first tab on page load
-    $('.nav-tabs a:first').tab('show');
-  
-    // Switch to the tab that was clicked
-    $('.nav-tabs a').on('click', function(e) {
-      $(this).tab('show');
-    });
+  // Get the initial tab from the URL hash (e.g. #total). The hash value will be used to determine which is active initally.
+  var initialTab = window.location.hash;
+
+  // Activate the initial tab. Finds the href that matches the hash value. tab('show') activates that tab.
+  $('.nav-tabs a[href="' + initialTab + '"]').tab('show');
+
+  // Switch to the tab that was clicked. 
+  $('.nav-tabs a').on('click', function(e) {
+    // Update the title variable to the text of the clicked tab
+    title = $(this).text().trim();
+
+    // Update the URL hash to reflect the current tab.
+    window.location.hash = $(this).attr('href');
   });
+});
